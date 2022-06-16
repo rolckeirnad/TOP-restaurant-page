34c1a58429eea0b97ec52d7eebb450af1377065e
Daniel Gallardo <dgallardo.c@outlook.com>
2022-06-16 00:47:47 -0500

    Add base files and add listeners to elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Webpack test");
+import './index.css';
+
+const mainContent = document.querySelector('#content');
+const navOptions = document.querySelectorAll('#mainNav li');
+
+for (let option of navOptions) option.addEventListener('click', testFunction);
+
+mainContent.classList.add('dummyContent');
+mainContent.textContent = "Changed with js";
+
+function testFunction() {
+    console.log("I was clicked");
+}
