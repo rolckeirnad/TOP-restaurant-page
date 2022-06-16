5a92cf77a48b1926e0c0fe540fad3f1b0a81a3fd
Daniel Gallardo <dgallardo.c@outlook.com>
2022-06-16 00:45:30 -0500

    Add support for separate css files

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
