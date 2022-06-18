2a14244fb8d4205eac9327d28c98d821bac02761
Daniel Gallardo <dgallardo.c@outlook.com>
2022-06-18 01:16:07 -0500

    Add image and styles to nav bar

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import './index.css';
 
 const mainContent = document.querySelector('#content');
 const navOptions = document.querySelectorAll('#mainNav li');
+const logoContainer = document.querySelector('#navLogoContainer');
 
-for (let option of navOptions) option.addEventListener('click', testFunction);
+const navLogo = new Image();
+navLogo.src = Logo;
 
+const logoName = document.createElement('span');
+logoName.innerText = "Burrito Burger";
+
+logoContainer.appendChild(navLogo);
+logoContainer.appendChild(logoName);
 mainContent.classList.add('dummyContent');
 mainContent.textContent = "Changed with js";
 
